655d3e76b916bf4b3b6ef85495096949849b1798
saikrishna.pawar <saikrishna.pawar@raybiztech.com>
2016-05-12 14:36:03 +0530

    Initial Commit

---
+++ app/src/main/java/org/rbt/androidrecyclerviewexample/CustomViewHolder.java
@@ -0,0 +1,20 @@
+package org.rbt.androidrecyclerviewexample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by saikrishna.pawar on 29/12/15.
+ */
+public class CustomViewHolder extends RecyclerView.ViewHolder {
+    protected ImageView imageView;
+    protected TextView textView;
+
+    public CustomViewHolder(View view) {
+        super(view);
+        this.imageView = (ImageView) view.findViewById(R.id.thumbnail);
+        this.textView = (TextView) view.findViewById(R.id.title);
+    }
+}
---
+++ app/src/main/java/org/rbt/androidrecyclerviewexample/FeedItem.java
@@ -0,0 +1,26 @@
+package org.rbt.androidrecyclerviewexample;
+
+/**
+ * Created by saikrishna.pawar on 29/12/15.
+ */
+public class FeedItem {
+
+    private String title;
+    private String thumbNail;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbNail() {
+        return thumbNail;
+    }
+
+    public void setThumbNail(String thumbNail) {
+        this.thumbNail = thumbNail;
+    }
+}
